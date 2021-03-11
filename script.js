7ee68918b9c4939e1ba7e7d3b7fe069458dbaee3
fatpumpkin <beaulieusimon30@gmail.com>
2021-03-11 10:38:15 -0500

    jour2

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 let e = true;
-function togglesection(){
-    if(e == true){
+function togglesection() {
+    if (e == true) {
         document.getElementById("toggle").style.display = "block";
         document.getElementById("explore").innerHTML = "Abandon";
         document.getElementById("bas").style.top = "1122px";
-        e = false;   
+        e = false;
     }
-    else if(e == false){
+    else if (e == false) {
         document.getElementById("toggle").style.display = "none";
         document.getElementById("explore").innerHTML = "Start exploring";
         document.getElementById("bas").style.top = "622px";
-        e = true;   
+        e = true;
     }
+}
+
+let charactername = ["Meredith", "Robert", "Kirael", "Jean", "Johnny", "Kathleen", "Aria", "Alberto", "Antonio", "Eralaya"];
+let characterclass = ["Wizard", "Paladin", "Necromancer", "Fighter", "Rogue"];
+let race = ["Human", "Orc", "Elf", "Dwarf", "Tiefling"];
+let specialisation = [];
+let i = "";
+let i2 = "";
+
+function randomizer() {
+    i = charactername[Math.floor(Math.random() * charactername.length)];
+    i2 = charactername[charactername.length - i];
+    alert(i2);
 }
