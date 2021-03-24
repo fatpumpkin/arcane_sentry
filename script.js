29753ffb8a90f179502981c1ac7f5ac1e9275cec
fatpumpkin <beaulieusimon30@gmail.com>
2021-03-24 14:35:13 -0400

    jour3

--- script.js
+++ script.js
@@ -52,3 +52,26 @@ function randomizer() {
     cspecialisation = specialisation[Math.floor(Math.random() * specialisation.length)];
     $("#result").html("<p>" + cname + "</p> <p>" + crace + "</p> <p>" + cclass + "</p> <p>" + cspecialisation + "</p>")
 }
+
+$(document).ready(function () {
+
+    $("#cclass").change(function () {
+        var val = $(this).val();
+        if (val == "") {
+            $("#specialisation").html("<option value='0'>Select a specialisation</option>");
+        } else if (val == "wizard") {
+            $("#specialisation").html("<option value='0'>Select a specialisation</option> <option value='scribe'>Scribe</option><option value='arcanist'>Arcanist</option><option value='summoner'>Summoner</option><option value='pyromancer'>Pyromancer</option><option value='cryomancer'>Cry-Oh-mans-her</option>");
+
+        } else if (val == "paladin") {
+            $("#specialisation").html("<option value='0'>Select a specialisation</option> <option value='crusader'>Crusader</option><option value='divineguardian'>Divine Guardian</option><option value='oathbound'>Oathbound</option><option value='holytactician'>Holy Tactitian</option>");
+        } else if (val == "necromancer") {
+            $("#specialisation").html("<option value='0'>Select a specialisation</option> <option value='necrolord'>Necrolord</option><option value='soulstitcher'>Soul Stitcher</option><option value='keeperofsouls'>Keeper of Souls</option><option value='plaguespreader'>Plague Spreader</option>");
+
+        } else if (val == "fighter") {
+            $("#specialisation").html("<option value='0'>Select a specialisation</option> <option value='champion'>Champion</option><option value='weaponmaster'>Weapon Master</option><option value='duelist'>Duelist</option><option value='eldritchwarrior'>Eldritch Warrior</option><option value='samurai'>Samurai</option><option value='cavalier'>Cavalier</option>");
+
+        } else if (val == "rogue") {
+            $("#specialisation").html("<option value='0'>Select a specialisation</option> <option value='assassin'>Assassin</option><option value='scout'>Scout</option><option value='swashbuckler'>Swashbuckler</option><option value='thief'>Thief</option><option value='arcanetrickster'>Arcane Trickster</option>");
+
+        }
+    }});
